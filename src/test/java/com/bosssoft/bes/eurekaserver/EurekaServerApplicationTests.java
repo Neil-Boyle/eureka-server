387bd898396c7f3c445000573de43849bab5f5fe
Neil-Boyle <>
2019-12-03 16:17:35 +0800

    init commit with eureka-server config

---
+++ src/test/java/com/bosssoft/bes/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bosssoft.bes.eurekaserver;
+
+import org.junit.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication()
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
